fbab82d4e1036111a535aa11284ce665d99eeff5
Jesse Baird <jebaird@gmail.com>
2013-04-04 12:35:59 -0500

    tried setting basepath via steal root

--- ckeditor.js
+++ ckeditor.js
@@ -2,9 +2,23 @@
  * var CKEDITOR_BASEPATH = '/js/ckeditor/lib/';
  * should work but steal removeds it when it builds it sooooo put it the ckeditor.js lib
  */
-steal({
+steal(
+	/*
+     * 	try to set the basepath based on the steal root
+     */
+	function(){
+		var root = steal.root;
+		
+		var path = root.path.replace( root.domain(), '' )
+					.replace( root.protocol() + '//', '' );
+		
+		//make sure this is in the window			
+		CKEDITOR_BASEPATH = path + 'ckeditor/lib/';
+		window.CKEDITOR_BASEPATH = CKEDITOR_BASEPATH;
+	},
+	{
 		src: './lib/ckeditor.js',
 		//ie doesnt like this in prodction.js
 		packaged: true
-	})
-
+	}
+);
